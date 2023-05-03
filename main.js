20e5c95d0b6f7f5dbbce19732747a1d78804f6f1
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2023-05-03 16:02:26 +0200

    render

--- main.js
+++ main.js
@@ -55,3 +55,46 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+//SELEZIONI
+const eleContainer = document.querySelector("#container")
+render()
+//RENDER
+function render (){
+    eleContainer.innerHTML = ' ';
+    posts.forEach((element) => {
+        console.log(element.author['image'])
+        eleContainer.innerHTML += `<div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${element.author['image']}" alt="${element.author['image']}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${element.author['name']}</div>
+                    <div class="post-meta__time">4 mesi fa</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">
+        ${element.content}
+        </div>
+        <div class="post__image">
+            <img src="${element.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+    })
+
+}
